719109f64755cb80e657a8e2c1085610b0d4fd02
zzllg <296160709@qq.com>
2019-06-13 17:08:15 +0800

    Update NN_train.py

--- NN_train.py
+++ NN_train.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[16]:
+# In[ ]:
 
 
 import pandas as pd
@@ -14,14 +14,14 @@ from matplotlib import pyplot as plt
 np.set_printoptions(suppress = True)
 
 
-# In[57]:
+# In[ ]:
 
 
 data_others = pd.read_csv(r'samples_others.csv',header= None)
 data_201807 = pd.read_csv(r'samples_201807.csv',header= None)
 
 
-# In[58]:
+# In[ ]:
 
 
 # 剔除日期、晴雨指示
@@ -30,14 +30,27 @@ idx_t = list(range(19,112))
 idx_h = list(range(112,205))
 
 
-# In[59]:
+# In[ ]:
+
+
+def get_valloss(model,data,idx_1,idx_2,flag = 0):
+    a = data
+    b = model.predict(a[:,idx_1])
+    c = a[:,idx_2]
+    if flag:
+        return ((b - c)**2).mean(axis = 0)**0.5
+    else:
+        return ((b - c)**2).mean()**0.5
+
+
+# In[ ]:
 
 
 # 交叉验证
 data_train,data_test = train_test_split(data_others.values,test_size = 0,random_state = 10,shuffle = True)
 
 
-# In[73]:
+# In[ ]:
 
 
 # 创建模型
@@ -48,7 +61,7 @@ outputs = layers.Dense(93,activation='linear',name= 'outputs')(dropout_1)
 # hidden_2 = layers.Dense(32,activation='relu',name= 'hidden_2')(dropout_1)
 # dropout_2 = layers.Dropout(rate= 0.1,seed= 10,name= 'dropout_2')(hidden_2)
 # outputs = layers.Dense(93,activation='linear',name= 'T/H')(dropout_2)
-model = tf.keras.Model(inputs= inputs,outputs= outputs,name= 'model_T')
+model = tf.keras.Model(inputs= inputs,outputs= outputs,name= 'model_H')
 # 模型摘要
 model.summary()
 
@@ -59,43 +72,31 @@ model.compile(optimizer= tf.keras.optimizers.Adam(learning_rate= 0.001),
 
 # 训练
 history = model.fit(x= data_train[:,idx_bt],
-                    y= data_train[:,idx_t],
+                    y= data_train[:,idx_h],
                     batch_size= None,
-                    epochs= 1000,
+                    epochs= 4000,
 #                     validation_split= 0.1,
-                    validation_data=(data_201807.values[:,idx_bt],data_201807.values[:,idx_t]),
-#                     callbacks= [tf.keras.callbacks.EarlyStopping(patience= 100,restore_best_weights= True)],
+                    validation_data=(data_201807.values[:,idx_bt],data_201807.values[:,idx_h]),
+                    callbacks= [tf.keras.callbacks.EarlyStopping(patience= 500,restore_best_weights= True)],
                     verbose= 1)
 
 
-# In[76]:
+# In[ ]:
 
 
 # 保存模型
-model.save('model_save.h5')
+model.save('model_save_H.h5')
 # 加载模型
 del model
-model = tf.keras.models.load_model('model_save.h5')
+model = tf.keras.models.load_model('model_save_H.h5')
 
 
-# In[77]:
+# In[ ]:
 
 
-pred = model.predict(data_201807.values[:,idx_bt])
-y_test = data_201807.values[:,idx_t]
-rmse = ((pred - y_test)**2).mean(axis = 0)**0.5
 # 画图
+rmse = get_valloss(model,data_201807.values,idx_bt,idx_h,1)
 fig = plt.figure(1,(8,6),100)
-plt.plot(rmse,np.arange(1,94),'r-')
+plt.plot(rmse,np.arange(1,94),'b-o')
 plt.show()
-
-
-# In[78]:
-
-
-a = data_201807.values
-b = model.predict(a[:,idx_bt])
-c = a[:,idx_t]
-# ((b - c)**2).mean(axis = 0)**0.5
-((b - c)**2).mean()**0.5
 
