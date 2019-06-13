298765103a1b1a651a36470eaf3cd3f81a679da2
zzllg <296160709@qq.com>
2019-06-13 11:21:28 +0800

    Add files via upload

--- LV1_LV2_match.py
+++ LV1_LV2_match.py
@@ -1,6 +1,9 @@
 
 # coding: utf-8
 
+# In[1]:
+
+
 import pandas as pd
 import numpy as np
 import re
@@ -8,12 +11,23 @@ np.set_printoptions(suppress=True)
 pd.set_option('display.float_format', lambda x: '%.3f' %x)
 
 
+# In[2]:
+
+
 pathBt = r'F:\data\beijing2018\LV\LV1\00_12\2018_00_12.csv'
-pathSounding = r'F:\data\beijing2018\探空\插值后\2018_54511(插值后).xlsx'
+pathSounding = r'F:\data\beijing2018\探空\插值后\2018_54511(插值后)_long.xlsx'
 pathOut = r'F:\data\beijing2018\LV\train\samples.csv'
+
+
+# In[3]:
+
 
 dataBt = pd.read_csv(pathBt,header=None).T
 dataSounding = pd.read_excel(pathSounding,header=None).T
+
+
+# In[4]:
+
 
 def transform(date):
     date = re.sub(r'/',r'-',date) 
@@ -25,6 +39,10 @@ def transform(date):
     else:
         print('None')
     return date
+
+
+# In[5]:
+
 
 columns = []
 samples = []
@@ -44,5 +62,30 @@ for i,date in enumerate(dates):
 samples = np.array(samples).astype('float')
 samples = samples.round(2)
 
+
+# In[6]:
+
+
 pd.DataFrame(samples,index= columns).to_csv(pathOut,header = None)
+
+
+# In[ ]:
+
+
+# 汇总处理
+data = pd.read_csv(pathOut,header= None)
+
+# 挑出2018-07的数据，后期要对比，不参与训练
+dates = data[0].values
+idx_201807 = []
+idx_others = []
+for i in range(len(dates)):
+    if dates[i][:7] == '2018-07':
+        idx_201807.append(i)
+    else:
+        idx_others.append(i)
+
+# 保存
+data.iloc[idx_201807].to_csv(r'samples_201807.csv',index = None,header = None)
+data.iloc[idx_others].to_csv(r'samples_others.csv',index = None,header = None)
 
--- NN_train.py
+++ NN_train.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[16]:
 
 
 import pandas as pd
@@ -10,37 +10,18 @@ from tensorflow.keras import layers
 import numpy as np
 # import re
 from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 np.set_printoptions(suppress = True)
 
 
-# In[138]:
+# In[57]:
 
 
-# 汇总处理
-data = pd.read_csv(r'F:\data\beijing2018\LV\train\samples.csv',header= None)
-
-# 挑出2018-07的数据，后期要对比，不参与训练
-dates = data[0].values
-idx_201807 = []
-idx_others = []
-for i in range(len(dates)):
-    if dates[i][:7] == '2018-07':
-        idx_201807.append(i)
-    else:
-        idx_others.append(i)
-
-# 保存
-data.iloc[idx_201807].to_csv(r'F:\data\beijing2018\LV\train\samples_201807.csv',index = None,header = None)
-data.iloc[idx_others].to_csv(r'F:\data\beijing2018\LV\train\samples_others.csv',index = None,header = None)
+data_others = pd.read_csv(r'samples_others.csv',header= None)
+data_201807 = pd.read_csv(r'samples_201807.csv',header= None)
 
 
-# In[139]:
-
-
-data = pd.read_csv(r'F:\data\beijing2018\LV\train\samples_others.csv',header= None)
-
-
-# In[140]:
+# In[58]:
 
 
 # 剔除日期、晴雨指示
@@ -49,35 +30,72 @@ idx_t = list(range(19,112))
 idx_h = list(range(112,205))
 
 
-# In[210]:
+# In[59]:
 
 
 # 交叉验证
-data_train,data_test = train_test_split(data.values,test_size = 0.1,random_state = 10,shuffle = True)
+data_train,data_test = train_test_split(data_others.values,test_size = 0,random_state = 10,shuffle = True)
+
+
+# In[73]:
+
 
 # 创建模型
-inputs = tf.keras.Input(shape= (17,),name= 'bt')
-hidden_1 = layers.Dense(32,activation='relu',name= 'hidden_1')(inputs)
+inputs = tf.keras.Input(shape= (17,),name= 'inputs')
+hidden_1 = layers.Dense(48,activation='linear',name= 'hidden_1')(inputs)
 dropout_1 = layers.Dropout(rate= 0.1,seed= 10,name= 'dropout_1')(hidden_1)
-outputs = layers.Dense(93,activation='linear',name= 'T/H')(dropout_1)
+outputs = layers.Dense(93,activation='linear',name= 'outputs')(dropout_1)
 # hidden_2 = layers.Dense(32,activation='relu',name= 'hidden_2')(dropout_1)
 # dropout_2 = layers.Dropout(rate= 0.1,seed= 10,name= 'dropout_2')(hidden_2)
 # outputs = layers.Dense(93,activation='linear',name= 'T/H')(dropout_2)
-model = tf.keras.Model(inputs= inputs,outputs= outputs,name= 'mymodel')
+model = tf.keras.Model(inputs= inputs,outputs= outputs,name= 'model_T')
 # 模型摘要
 model.summary()
 
 # 模型编译
 model.compile(optimizer= tf.keras.optimizers.Adam(learning_rate= 0.001),
-              loss= tf.losses.MeanSquaredError(),
+              loss= 'mean_squared_error',
               metrics= [])
 
 # 训练
 history = model.fit(x= data_train[:,idx_bt],
                     y= data_train[:,idx_t],
                     batch_size= None,
-                    epochs= 500,
+                    epochs= 1000,
 #                     validation_split= 0.1,
-                    validation_data=(data_test[:,idx_bt],data_test[:,idx_t]),
+                    validation_data=(data_201807.values[:,idx_bt],data_201807.values[:,idx_t]),
+#                     callbacks= [tf.keras.callbacks.EarlyStopping(patience= 100,restore_best_weights= True)],
                     verbose= 1)
+
+
+# In[76]:
+
+
+# 保存模型
+model.save('model_save.h5')
+# 加载模型
+del model
+model = tf.keras.models.load_model('model_save.h5')
+
+
+# In[77]:
+
+
+pred = model.predict(data_201807.values[:,idx_bt])
+y_test = data_201807.values[:,idx_t]
+rmse = ((pred - y_test)**2).mean(axis = 0)**0.5
+# 画图
+fig = plt.figure(1,(8,6),100)
+plt.plot(rmse,np.arange(1,94),'r-')
+plt.show()
+
+
+# In[78]:
+
+
+a = data_201807.values
+b = model.predict(a[:,idx_bt])
+c = a[:,idx_t]
+# ((b - c)**2).mean(axis = 0)**0.5
+((b - c)**2).mean()**0.5
 
